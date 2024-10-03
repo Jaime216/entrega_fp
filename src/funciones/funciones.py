465aaaa1b5d7c73cf0031aa047a73f6ffae8aa55
Jaime216 <jcgonzalez7406@gmail.com>
2024-10-03 19:27:41 +0200

    Funciones 1, 2, 3, 4 y 5

--- src/funciones/funciones.py
+++ src/funciones/funciones.py
@@ -1,1 +1,52 @@
+def funcion_1( n: int, k: int ):
+    resultado: int = 1
+    for i in range( k ):
+        resultado *= ( n - i + 1 )
+    return resultado
+
+
+def funcion_2( r: int, a_1: int, k:int ):
+    res = 1
+    for i in range( 1, k + 1 ):
+        res *= a_1 * r ** ( i - 1 )
+    return res
+
+
+def factorial( n:int ):
+    res = 1
+    for i in range( n ):
+        res *= i + 1
+    return res
+
+
+def funcion_3( n:int, k:int ):
+# n >= k
+    numero_combinatorio = factorial( n ) // ( factorial( k ) * factorial( ( n - k ) ) )
+    return numero_combinatorio
+
+
+def funcion_4( n:int, k:int ):
+# n >= k
+    sumatorio = 0
+    for i in range( k ):
+        sumatorio += ( -1 ) ** i * funcion_3( k + 1, i + 1 ) * ( k - i ) ** n
+    return ( 1 / factorial( k ) ) * sumatorio
+
+
+def funcion_5( a: float, error: float ):
+
+    def f( x ):return  2 * x ** 2
+
+    def der( x ):return 4 * x
+
+    xn = a
+    for i in range( 1000 ):
+        if( abs( f( xn ) ) <= error ):
+            print( f( xn ) , i )
+            break
+        else:
+            print( f( xn ), der( xn ) )
+            xn = xn - f( xn ) / der( xn )
+            print( xn, i )
+    return xn
 
---
+++ src/tests/TestFunctions.py
@@ -0,0 +1,54 @@
+from funciones.funciones import funcion_1, funcion_3, funcion_2, funcion_4
+
+
+def test_funcion_1( n:int , k:int ):
+    res = funcion_1( n, k )
+    print( f"""################################################
+TEST DE LA FUNCIÓN 1:
+El producto de {n} y {k} es: {res}
+""" )
+
+
+test_funcion_1( 4, 2 )
+
+
+def test_funcion_2( a_1:int, r:int, k:int ):
+    res = funcion_2( a_1 = a_1, r = r, k = k )
+    print( f"""################################################
+TEST DE LA FUNCIÓN 2:
+El producto de la secuencia geométrica con a1 = {a_1}, r = {r} y k = {k} es: {res}""" )
+
+
+test_funcion_2( a_1 = 3, r = 5, k = 2 )
+
+
+def test_funcion_3( n:int, k:int ):
+    res = funcion_3( n, k )
+    print( f"""
+################################################
+TEST DE LA FUNCIÓN 3:
+El número combinatorio de {n} y {k} es: {res}
+""" )
+
+
+test_funcion_3( 4, 2 )
+
+
+def test_funcion_4( n: int, k:int ):
+    res = funcion_4( n, k )
+    print( f"""################################################
+TEST DE LA FUNCIÓN 4:
+El número S(n,k) siendo n = {n} y {k} = 2 es: {res}""" )
+
+
+test_funcion_4( 4, 2 )
+
+
+def test_funcion_5( a: float, error: float ):
+    res = funcion_5( a, error )
+    print( f"""################################################
+TEST DE LA FUNCIÓN 5:
+Resultado de la función 5 con a = {a} y e = {e}, f(x) = 2x^2 y f'(x) = 4x: {res}""" )
+
+
+test_funcion_5( 3, 0.001 )
