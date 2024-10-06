267d68d554ba97e050febe9855d8d38abf7511b7
Jaime216 <jcgonzalez7406@gmail.com>
2024-10-06 16:34:01 +0200

    Entrega 1 terminada

--- src/lecturas/lecturas.py
+++ src/lecturas/lecturas.py
@@ -1,1 +1,58 @@
+# Description: Funciones de lectura de archivos
+import csv
+from typing import Optional
 
+
+def funcion_6( file_name: str, delimitador:str, word: str ):
+    apariciones = 0
+    with open( file_name, encoding = 'UTF-8' ) as file:
+        for line in file:
+            words = line.split( delimitador )
+            for w in words:
+                if( w.strip().lower() == word.lower() ): apariciones += 1
+    return apariciones
+
+
+def funcion_7( file_name: str, word: str ):
+    lines = []
+    with open( file_name, encoding = 'UTF-8' ) as file:
+        for line in file:
+            if word.lower() in line.lower(): lines.append( line )
+    return lines
+
+
+def funcion_8( file_name: str ):
+    txt = ""
+    words = []
+    res = []
+    with open( file_name, encoding = 'UTF-8' ) as file:
+        for line in file:
+            line.replace( "\n", "" )
+            txt += line
+            words += line.replace( "\n", "" ).split( " " )
+    for word in words:
+        count = 0
+        ls = txt.split( ' ' )
+        for l in ls:
+            if l.lower().strip() == word.lower().strip() and ( l in res ) == False:
+                count += 1
+        if count == 1:
+            res.append( word )
+        else:
+            count = 0
+
+    return res
+
+
+def longitud_promedio_lineas( file_path: str ) -> Optional[float]:
+    lines = 0
+    words = 0
+    with open( file_path, mode = 'r', newline = '', encoding = 'utf-8' ) as archivo_csv:
+        lector_csv = csv.reader( archivo_csv )
+        # Iteramos sobre cada fila del archivo
+        for line in lector_csv:
+                lines += 1
+                words += len( line )
+    print( lines, words )
+    if( lines == 0 ): return None
+    else: return words / lines
---
+++ src/tests/TestLecturas.py
@@ -0,0 +1,44 @@
+from lecturas.lecturas import funcion_6, funcion_7, funcion_8, longitud_promedio_lineas
+
+
+def test_funcion_6( file_name: str, delimitador: str, word: str ):
+    res = funcion_6( file_name, delimitador, word )
+    print( f"""################################################
+TEST DE LA FUNCIÓN 6:
+El número de veces que aparece la palabra {word} en el fichero {file_name.strip("/")[3]} es: {res}
+""" )
+
+
+test_funcion_6( "../../resources/lin_quijote.txt", " ", "quijote" )
+
+
+def test_funcion_7( file_name: str, word: str ):
+    list = funcion_7( file_name, word )
+    print( f"""################################################
+TEST DE LA FUNCIÓN 7:
+Las líneas en las que aparece la palabra {word} son: {list}
+    """ )
+
+
+test_funcion_7( "../../resources/lin_quijote.txt", "quijote" )
+
+
+def test_funcion_8( file_name: str ):
+    res = funcion_8( file_name )
+    print( f"""################################################
+TEST DE LA FUNCIÓN 8:
+Las palabras únicas en el fichero {file_name} son: {res}""" )
+
+
+test_funcion_8( "../../resources/archivo_palabras.txt" )
+
+
+def test_funcion_9( file_name: str ):
+    res = longitud_promedio_lineas( file_name )
+    print( f"""################################################
+TEST DE LA FUNCIÓN 9:
+La longitud promedio de las líneas del fichero {file_name} es: {res}""" )
+
+
+test_funcion_9( "../../resources/vacio.csv" )
+test_funcion_9( "../../resources/palabras_random.csv" )
